033825864442ec3e7d5e3e2ad90b3dcbcfec6c63
Josefina Castro Dintrans <castro.josefina@uc.cl>
2025-10-24 13:24:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const portfolio = document.querySelector("#holahola");
+
+async function datos(raw) {
+    try {
+        let consulta = await fetch(raw);
+        let resultado = await consulta.json();
+        let trabajos = resultado.data;
+        console.log(trabajos);
+        trabajos.forEach((trabajo) => {
+            portfolio.innerHTML += `
+
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                <img src="${trabajo.imagen}" class="card-img-top" alt="${trabajo.titulo}">
+                                <div class="card-body">
+                                <p class="card-text">${trabajo.titulo}</p>
+                                <div class="d-flex
+                                justify-content-between
+                                align-items-center">
+                                <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.categoria}</button>
+                                </div>
+                                <small class="text-body-secondary">Reciente </small>
+             </div>
+             </div>
+            </div>
+            </div>`;
+        });
+    } catch (error) {
+        console.error("Error al cargar los datos:",error);
+}
+}
+datos("https://api.myjson.online/v1/records/492fb163-7104-442b-a654-890e658d549b")
+
